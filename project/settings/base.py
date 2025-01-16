8b9283abe137610d05985fd145897bdf1af4eb2d
souzaoliv <chamaodevme@gmail.com>
2025-01-16 08:58:34 -0300

    Primeiro Commit

--- project/settings/base.py
+++ project/settings/base.py
@@ -29,9 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
