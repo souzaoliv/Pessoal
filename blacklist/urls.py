4f4cc17164f74f44bea573978fcabbe16fb154fb
souzaoliv <chamaodevme@gmail.com>
2025-01-20 03:02:46 -0300

    adicionado rota index

--- blacklist/urls.py
+++ blacklist/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.http import HttpResponse
+from .views import IndexView
 
+app_name = 'blacklist'
 
 urlpatterns = [
-    path('', lambda request: HttpResponse('Blacklist')),
+    path('', IndexView.as_view(), name='index'),
 ]
