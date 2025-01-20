29fd182b3e427024a38f6d57694190a75f1be5d7
souzaoliv <chamaodevme@gmail.com>
2025-01-20 03:39:58 -0300

    login usuario

--- blacklist/urls.py
+++ blacklist/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.http import HttpResponse
-from .views import IndexView, RegisterView
+from .views import IndexView, RegisterView, LoginView
 
 app_name = 'blacklist'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('register/', RegisterView.as_view(), name='register'),
-
+    path('login/', LoginView.as_view(), name='login'),
+   
 ]
--- blacklist/views.py
+++ blacklist/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, View
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -20,3 +21,16 @@ class RegisterView(View):
             login(request, user)
             return redirect('blacklist:index')
         return render(request, 'blacklist/register.html', {'form': form})
+
+class LoginView(View):
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, 'blacklist/login.html', {'form': form})
+    
+    def post(self, request):
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return HttpResponse('logado')
+        return render(request, 'blacklist/login.html', {'form': form})
