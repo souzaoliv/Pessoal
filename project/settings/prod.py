a006110c53e54f7c4d275ca5a7d39e371b0fac2b
souzaoliv <chamaodevme@gmail.com>
2025-01-27 11:06:03 -0300

    v2

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -6,4 +6,10 @@ from project.settings.base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',          # Para quando o app estiver rodando localmente
+    'https://lucasgso.com.br',        # Para o domínio principal com HTTPS
+    'https://www.lucasgso.com.br'     # Para o subdomínio com HTTPS
+]
+
 ALLOWED_HOSTS = ['*']
